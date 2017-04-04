6e57b87555563e6649aba89c0b5cac8a99ad2ac9
Andy Collins <askingalot@gmail.com>
2017-04-03 21:16:15 -0500

    Csv sort_by method

--- src/csv_sorter.rs
+++ src/csv_sorter.rs
@@ -7,7 +7,17 @@ pub struct Csv {
 impl Csv {
     pub fn new(mut rows: Vec<Vec<String>>) -> Csv {
         let header = rows.remove(0);
-        Csv { header: header, rows: rows }
+        Csv {
+            header: header,
+            rows: rows,
+        }
+    }
+
+    pub fn sort_by(&mut self, col_name: &String) {
+        let col_index = self.header.iter().position(|h| h == col_name).unwrap();
+
+        self.rows
+            .sort_by(|a, b| a[col_index].cmp(&(b[col_index])));
     }
 }
 
@@ -23,13 +33,12 @@ mod tests {
         let row2 = vec!["baz", "qux"];
         let csv_data = vec![header, row1, row2];
 
-        let string_csv_data =
-            csv_data
-                .iter()
-                .map(|row| row.iter().map(|col| col.to_string()).collect())
-                .collect();
+        let string_csv_data = csv_data
+            .iter()
+            .map(|row| row.iter().map(|col| col.to_string()).collect())
+            .collect();
 
-        Csv::new( string_csv_data )
+        Csv::new(string_csv_data)
     }
 
     #[test]
@@ -44,5 +53,15 @@ mod tests {
         let csv = build_csv();
 
         assert_eq!("Col1".to_string(), csv.header[0]);
+    }
+
+    #[test]
+    fn sort_by_col_name_sorts_csv() {
+        let mut csv = build_csv();
+
+        let col: &String = &"Col1".to_string();
+        csv.sort_by(col);
+
+        assert_eq!("baz", csv.rows[0][0]);
     }
 }
